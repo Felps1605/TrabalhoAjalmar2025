15c2694f1e1b7dfffff4732468f6251de813dd3f
marquejudo <davivmarques2007@gmail.com>
2025-07-14 19:21:47 -0300

    Update uf.h

--- uf.h
+++ uf.h
@@ -1,11 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "UF.h"
+void estados() {
+    int opuf;
+    do {
+        menuuf();
+        scanf("%d", &opuf);
+        faxineirojp();
+        switch (opuf) {
+            case 1:
+                inserir();
+                break;
+            case 4:mostraruf();break;
+            case 0:break;
+            default:
+                printf("escolha uma opcao valida \n");
+                break;
+        }
 
-struct UF{
-    int codigo;
-    char descricao[3];
-    char sigla[3];
-}; 
+    } while (opuf != 0);
 
-uf_inserir();
-uf_alterar();
-uf_deletar();
-uf_ler()
+
+}
+void menuuf() {
+    printf("|============================================|\n");
+    printf("| 1- INSERIR                                 |\n");
+    printf("| 2- ALTERAR                                 |\n");
+    printf("| 3- DELETAR                                 |\n");
+    printf("| 4- LISTAR ESTADOS                          |\n");
+    printf("| 5- MOSTRAR ESTADO                          |\n");
+    printf("| 0- SAIR                                    |\n");
+    printf("|============================================|\n");
+}
+
+void inserir() {
+    FILE *f = fopen("UF.data", "rb+");
+    if (f == NULL) {
+        f = fopen("UF.data", "wb+");
+        if (f == NULL) {
+            printf("Erro ao abrir arquivo UF.data\n");
+            exit(-1);
+        }
+    }
+
+    uf temp;
+    int cod;
+    printf("digite o codigo: \n");
+    scanf("%d", &cod);
+    faxineirojp();
+    temp.codigo = cod;
+
+    char descri[100];
+    printf("insira a descriçao: \n");
+    fgets(descri, 100, stdin);
+    descri[strcspn(descri, "\n")] = '\0';
+    strcpy(temp.descricao,descri);
+
+    char sig[3];
+    printf("insira a sigla: \n");
+    fgets(sig, sizeof(sig), stdin);
+    sig[strcspn(sig, "\n")] = '\0';
+    strcpy(temp.sigla, sig);
+
+
+    fseek(f, 0, SEEK_END);
+    fwrite(&temp, sizeof(uf), 1, f);
+    fclose(f);
+
+}
+void mostraruf() {
+    FILE *f = fopen("UF.data", "rb+");
+    if (f == NULL) {
+        printf("Arquivo nao existe. \n");
+        return;
+    }
+    uf temp;
+    fseek(f, 0, SEEK_SET);
+    while (fread(&temp,sizeof(uf),1,f)) {//jp disse que era mais facil
+        printf("codigo :%d \n", temp.codigo);
+        printf("Estado : %s \n", temp.descricao);
+        printf("Sigla : %s \n", temp.sigla);
+
+    }
+
+}
+void faxineirojp() {
+    while (getchar() != '\n');
+}
