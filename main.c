a41a52094ee546f72ce3a50e0b1e85777c02e585
Felps1605 <felipe.eliascymrot@gmail.com>
2025-07-17 18:29:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "eleicoes.h"
 #include "UF.h"
 #include "pessoas.h"
 void estados();
+void pessoas();
 void menu();
 int main() {
     loadUFs();
     carregar_pessoas();
+    carregar_eleicoes();
     int op;
     do {
         menu();
         scanf("%d", &op);
         faxineirojp();
         switch (op) {
-            case 1:estados();break;
+            case 1:
+                estados();
+                break;
+            case 2:
+                eleicoes();
+                break;
+            case 3:
+                pessoas();
+                break;
             case 0:break;
             default:printf("Opcao invalida\n");
         }
