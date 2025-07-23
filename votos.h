037e4467d7bb76aa60486566cad19989455b4be6
marquejudo <davivmarques2007@gmail.com>
2025-07-23 20:16:58 -0300

    Update votos.h

--- votos.h
+++ votos.h
@@ -1,5 +1,4 @@
 
-
 #ifndef VOTOS_H
 #define VOTOS_H
 
@@ -11,6 +10,16 @@ typedef struct {
     int numero_candidato;
     char data_hora[20];
 }voto;
+
+typedef enum {
+    SAIR_VOTO = 0,
+    INSERIR_VOTO = 1,
+    LISTAR_VOTOS_POR_CANDIDATO = 2,
+    LISTAR_VOTOS_POR_ANO = 3,
+    MOSTRAR_COMPARECIMENTO_POR_ELEICAO = 4,
+    MOSTRAR_COMPARECIMENTO_POR_ANO = 5
+} OpcaoVoto;
+
 extern voto *pvo;
 void votos();
 void menu_voto();
